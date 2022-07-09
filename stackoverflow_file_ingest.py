be875880a194827d1d58681e9cbceff41a993867
dvannoy <dvannoy>
2022-07-09 21:35:22 +0000

    File ingest for posts

--- stackoverflow_file_ingest.py
+++ stackoverflow_file_ingest.py
@@ -1,9 +1,9 @@
 # Databricks notebook source
-# MAGIC %sh wget https://archive.org/download/stackexchange/stackoverflow.com-Users.7z -O /tmp/Users.7z
+#%sh wget https://archive.org/download/stackexchange/stackoverflow.com-Users.7z -O /tmp/Users.7z
 
 # COMMAND ----------
 
-# MAGIC %sh mv /tmp/Users.yz /dbfs/tmp/Users.7z
+#%sh mv /tmp/Users.yz /dbfs/tmp/Users.7z
 
 # COMMAND ----------
 
@@ -11,9 +11,9 @@
 
 # COMMAND ----------
 
-import py7zr
-with py7zr.SevenZipFile('/dbfs/tmp/Users.7z', mode='r') as z:
-    z.extractall(path='/dbfs/tmp')
+#import py7zr
+#with py7zr.SevenZipFile('/dbfs/tmp/Users.7z', mode='r') as z:
+#    z.extractall(path='/dbfs/tmp')
 
 # COMMAND ----------
 
@@ -21,15 +21,27 @@ with py7zr.SevenZipFile('/dbfs/tmp/Users.7z', mode='r') as z:
 
 # COMMAND ----------
 
-# MAGIC %sh wget https://archive.org/download/stackexchange/stackoverflow.com-Posts.7z -O /tmp/Posts.7z
+# %sh wget https://archive.org/download/stackexchange/stackoverflow.com-Posts.7z -O /tmp/Posts.7z
 
 # COMMAND ----------
 
+import requests
+
 url = 'https://archive.org/download/stackexchange/stackoverflow.com-Posts.7z'
-local_filename =  'posts.7z'
+local_filename =  '/dbfs/tmp/posts.7z'
 with requests.get(url, stream=True) as r:
     r.raise_for_status()
     with open(local_filename, 'wb') as f:
         for chunk in r.iter_content(chunk_size=8192):
             if chunk:
                 f.write(chunk)
+
+# COMMAND ----------
+
+import py7zr
+with py7zr.SevenZipFile('/dbfs/tmp/posts.7z', mode='r') as z:
+    z.extractall(path='/dbfs/tmp')
+
+# COMMAND ----------
+
+
