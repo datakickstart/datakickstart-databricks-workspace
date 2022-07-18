9e039dc96fb0c2667dbf0e2b2a562ca98648ecc0
dvannoy <dvannoy>
2022-07-18 23:31:19 +0000

    Add to data exploration

--- best_of_class_recruiting/nb_cu_data_exploration.py
+++ best_of_class_recruiting/nb_cu_data_exploration.py
@@ -27,6 +27,18 @@ display(area_df)
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC ## Series
+# MAGIC 1. Business key = series_id
+# MAGIC 2. Relationship to area, item, base, periodicity?
+# MAGIC 3. base_period text not consistent
+# MAGIC 4. Columns in series_title text
+# MAGIC 5. footnote_codes (double): all NaN
+# MAGIC 6. being_year (bigint): min = 1913, max = 2020
+# MAGIC 7. end_year (bigint): min = 1986, max = 2022
+
+# COMMAND ----------
+
 # Evaluate and explore area
 series_df = spark.read.parquet(raw_storage_base_path + "cu/series")
 display(series_df)
@@ -35,8 +47,42 @@ display(series_df)
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC ## Series
+# MAGIC ## Item
+# MAGIC 1. Business key = item_code
+# MAGIC 2. Bigints = display_level, sort_sequence
+# MAGIC 3. Selectable all equals T
+
+# COMMAND ----------
+
+# Evaluate and explore area
+item_df = spark.read.parquet(raw_storage_base_path + "cu/item")
+display(item_df)
+
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Base
+# MAGIC 1. Business key = base_code
+# MAGIC 2. 2 entries: Alternate and Current
+
+# COMMAND ----------
+
+# Evaluate and explore area
+base_df = spark.read.parquet(raw_storage_base_path + "cu/base")
+display(base_df)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Current
 # MAGIC 1. Business key = 
+
+# COMMAND ----------
+
+# Evaluate and explore area
+current_df = spark.read.parquet(raw_storage_base_path + "cu/current")
+display(current_df)
 
 # COMMAND ----------
 
