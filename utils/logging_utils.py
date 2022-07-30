1d7fb513c87f826c8bb2df3e4e2bf252db693b1f
dvannoy <dvannoy>
2022-07-30 19:24:19 +0000

    Add logging utils

---
+++ utils/logging_utils.py
@@ -0,0 +1,35 @@
+import time
+
+logger = None
+job_start_time = None
+
+
+def start_logging(job_name):
+    global job_start_time = time.perf_counter()
+    
+    spark_log4j = sc._jvm.org.apache.log4j
+    global logger = spark_log4j.LogManager.getLogger("pyspark_logger")
+    
+    logger.info(f"Starting logger for {job_name}")
+
+    
+def log_debug_message(message):
+    logger.debug(message)
+    
+    
+def log_informational_message(message):
+    logger.info(message)
+    
+
+def log_warning_message(message):
+    logger.warn(message)
+    
+
+def log_error_message(message):
+    logger.error(message)
+    
+
+def stop_logging(job_name):
+    job_end_time = time.perf_counter()
+    duration = str(job_end_time - job_start_time)
+    logger.info(f"Stopping logger for {job_name}; total duration {duration}")
