a2618068019469fee9cc9c702203be18647269d3
dvannoy <dvannoy>
2022-07-31 05:13:04 +0000

    Improve streaming demo

--- stackoverflow/stackoverflow_streaming.py
+++ stackoverflow/stackoverflow_streaming.py
@@ -1,56 +1,36 @@
 # Databricks notebook source
-##Define Variables
-
-confluentBootstrapServers = "pkc-41973.westus2.azure.confluent.cloud:9092"
-
-confluentApiKey = dbutils.secrets.get('demo', 'confluent-cloud-user')
-
-confluentSecret = dbutils.secrets.get('demo', 'confluent-cloud-password')
-
-confluentTopicName = "stackoverflow_post"
-
-
-##Import Library
-
 import pyspark.sql.functions as fn
 from pyspark.sql.functions import col
-
 from pyspark.sql.types import StructType, StringType
- 
 
-##Create Spark Readstream
+from utils.logging_utils import *
 
-post_df = (
-  spark
-  .readStream
-  .format("kafka")
-  .option("kafka.bootstrap.servers", confluentBootstrapServers)
-  .option("kafka.security.protocol", "SASL_SSL")
-  .option("kafka.sasl.jaas.config", "kafkashaded.org.apache.kafka.common.security.plain.PlainLoginModule required username='{}' password='{}';".format(confluentApiKey, confluentSecret))
-  .option("kafka.sasl.mechanism", "PLAIN")
-  .option("subscribe", confluentTopicName)
-  .option("startingOffsets", "earliest")
-  .option("failOnDataLoss", "false")
-  .load()
-  # .select(fn.col("value").cast(StringType()).alias("value"))
-)
+job_name = 'stackoverflow_streaming'
+logger = start_logging(spark, job_name)
 
-post_transformed_df = (post_df
-.withColumn('key', fn.col("key").cast(StringType()))
-  .withColumn('fixedValue', fn.expr("substring(value, 6, length(value)-5)"))
-  .select('topic', 'partition', 'offset', 'timestamp', 'timestampType', 'key', 'value', 'fixedValue')
-)
+topic = "stackoverflow_post"
 
-# query = (post_transformed_df.writeStream
-#   .option("checkpointLocation", f"abfss://raw@datakickstartadls.dfs.core.windows.net/checkpoints/so_checkpoint_{version}")
-#   .format("delta")
-#   .outputMode("append")
-#   .trigger(processingTime="5 seconds")
-#   .start("abfss://raw@datakickstartadls.dfs.core.windows.net/stackoverflow_streaming")
-# )
 
-# query.processAllAvailable()
-# query.stop()
+def get_event_hub_config(topic):
+    # Password is really a Event Hub connection string, for example ->
+    # Endpoint=sb://<namespace>.servicebus.windows.net/;SharedAccessKeyName=ReadWriteTmp;SharedAccessKey=vhNXxXXXXXxxxXXXXXXXxx=;EntityPath=demo-message-1
+    password = dbutils.secrets.get(scope = "demo", key = "eh-sasl-{0}".format(topic))
+
+    EH_SASL = 'kafkashaded.org.apache.kafka.common.security.plain.PlainLoginModule required username="$ConnectionString" password="{0}";'.format(password)
+
+    config = {
+      'kafka.bootstrap.servers': 'dustin-demo-eh.servicebus.windows.net:9093',
+      'kafka.security.protocol': 'SASL_SSL',
+      'kafka.sasl.mechanism': 'PLAIN',
+      'kafka.group.id': GROUP_ID,
+      'kafka.request.timeout.ms': "60000",
+      'kafka.session.timeout.ms': "20000",
+      'kafka.heartbeat.interval.ms': "10000",
+      'kafka.sasl.jaas.config': EH_SASL,
+      'subscribe': topic
+    }
+    return config
+
 
 
 # COMMAND ----------
@@ -77,20 +57,49 @@ post_schema = (
     .add('_Body','string')
     .add('_ClosedDate','timestamp')
     .add('_CommentCount','long')
+    .add('_CommunityOwnedDate','timestamp')
+    .add('_ContentLicense','string')
+    .add('_CreationDate','timestamp')
+    .add('_FavoriteCount','long')
 )
 
-# _CommunityOwnedDate:timestamp
-# _ContentLicense:string
-# _CreationDate:timestamp
-# _FavoriteCount:long 
+# COMMAND ----------
 
+##Create Spark Readstream
+config = get_event_hub_config(topic)
+
+post_df = (
+  spark
+  .readStream
+  .format("kafka")
+  .options(**config)
+  .load()
+)
+
+post_transformed_df = (post_df
+.withColumn('key', fn.col("key").cast(StringType()))
+  .select('topic', 'partition', 'offset', 'timestamp', 'timestampType', 'key', 'value')
+)
+
+
+# COMMAND ----------
 
 df_parsed = post_transformed_df.select(
     col('value').cast('string').alias("value_str"),
     fn.from_json(col('value').cast('string'), post_schema).alias("json"))
-df = df_parsed.selectExpr("json.*")
+
+df_post = df_parsed.selectExpr("json.*")\
+
+
+# COMMAND ----------
+
+new_users = spark.read.delta("dbfs:/mnt/datakickstart/refined/stackoverflow_new_users").load()
+old_users = spark.read.parquet("dbfs:/mnt/datakickstart/raw/stackoverflow/users").load()
+all_users = old_users.union(new_users)
+
+df_combined = df_post.join(all_users, df_post["owner.user_id"] == all_users["user_id"], how="left")
 display(df)
 
 # COMMAND ----------
 
-
+stop_logging(job_name)
